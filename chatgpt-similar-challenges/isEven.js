1ba411f2060b3a013a4f2b7d9909f214341a515a
Charleskojomark <markcharleskojo@gmail.com>
2025-02-21 15:31:05 +0100

    iseven

---
+++ chatgpt-similar-challenges/isEven.js
@@ -0,0 +1,11 @@
+// Question
+// Write a function isEven(num) that returns true if a number is even and false otherwise.
+function isEven(num){
+    if(num % 2 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(isEven(45));
