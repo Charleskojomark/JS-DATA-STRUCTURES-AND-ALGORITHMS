ec309facb020d43bd7b0396871758fc05c3f014f
Charleskojomark <markcharleskojo@gmail.com>
2025-02-22 15:45:45 +0100

    findmax

---
+++ chatgpt-similar-challenges/findMax.js
@@ -0,0 +1,14 @@
+// Question
+// Write a function findMax(arr) that returns the largest number in an array.
+
+function findMax(arr){
+    let max = arr[0];
+    for(i = 0; i < arr.length; i++){
+        if (max < arr[i]){
+            max = arr[i]
+        }
+    };
+    return max;
+};
+
+console.log(findMax([7,12, 1, 3, 5,9, 4, 3, 8]));
