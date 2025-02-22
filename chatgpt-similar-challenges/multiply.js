fb67abe90b30fc443a050b5db3ceede9e148ce96
Charleskojomark <markcharleskojo@gmail.com>
2025-02-22 15:01:37 +0100

    multiply function

---
+++ chatgpt-similar-challenges/multiply.js
@@ -0,0 +1,8 @@
+// Question
+// Write an arrow function multiply(a, b) that takes two numbers and returns their product.
+
+const multiply = (a, b)=>{
+    return a * b
+}
+
+console.log(multiply(2, 3));
